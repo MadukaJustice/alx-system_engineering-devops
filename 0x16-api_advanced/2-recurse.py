81291b9b564dbd80529251beb8b09e88a4050a32
MadukaJustice <jonasjustice98@gmail.com>
2023-05-09 22:03:58 +0000

    update:recursive function

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,39 +1,43 @@
 #!/usr/bin/python3
+"""2-recurse
 """
-    Task 2
-"""
+import json
+
 import requests
+
+BASE_URL = "https://www.reddit.com/r"
+HEADERS = {"User-Agent": "ALX-Bot"}
 
 
 def recurse(subreddit, hot_list=[], after=None):
+    """recursively traverse all the pages of hot posts of a subreddit and
+    return the list of titles
+
+    Args:
+        subreddit (str): title of the subreddit
+        hot_list (list, optional): list of title of hot posts. Defaults to [].
+        after (str, optional): id of the previous page. Defaults to None.
+
+    Returns:
+        list or None: list of titles or None
     """
-        Returns the number of hot posts for a given subreddit.
-        Returns 0 if invalid subreddit was given
-    """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-            "User-Agent": "redditdev scraper by u/MadukaJustice",
-            "From": "jonasjustice98@gmail.com"
-    }
-    params = {
-            "limit": 100
-    }
+    query = "" if after is None else "?after={}".format(after)
+    result = requests.get(
+        "{}/{}/hot.json{}".format(BASE_URL, subreddit, query),
+        headers=HEADERS,
+        allow_redirects=False,
+    )
 
-    if after:
-        params["after"] = after
-
-    res = requests.get(url, headers=headers, params=params)
-    if res.status_code == 200:
-        data = res.json()
-
-        hot_posts = data.get("data", {}).get("children", [])
-        for post in hot_posts:
-            hot_list += post.get("data").get("title")
-
-        update_after = data.get("data").get("after")
-
-        if update_after:
-            return recurse(subreddit, hot_list, update_after)
-    else:
-        return None
-    return hot_list
+    if result.status_code == 200:
+        data = json.loads(result.text)["data"]
+        for post in data["children"]:
+            hot_list.append(post["data"]["title"])
+        after = data["after"]
+        return hot_list if after is None else recurse(
+            subreddit,
+            hot_list,
+            after,
+        )
+    elif len(hot_list) > 0:
+        return hot_list
+    return None
